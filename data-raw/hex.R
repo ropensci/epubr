50518381fa0ceda1c437b60edc5ccca39702ad75
leonawicz <matt.rstat@gmail.com>
2018-05-28 17:02:18 -0600

    Update color.

--- data-raw/hex.R
+++ data-raw/hex.R
@@ -7,7 +7,7 @@ out <- paste0(c("data-raw/", "inst/"), pkg, ".png")
 
 hex_plot <- function(out, mult = 1){
   sticker(img, 1, 1, 0.9, 1, "", h_size = mult * 1.2, h_fill = "black",
-          h_color = "white", url = paste0("leonawicz.github.io/", pkg), u_color = "white", u_size = mult * 3,
+          h_color = "gray", url = paste0("leonawicz.github.io/", pkg), u_color = "white", u_size = mult * 3,
           filename = out)
   # overwrite file for larger size
   if(mult != 1) ggsave(out, width = mult*43.9, height = mult*50.8, bg = "transparent", units = "mm")
